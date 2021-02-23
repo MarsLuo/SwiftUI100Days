2eb937c71412a32b2a7c75e97a85856a8df67116
LuoYang <ydluo@thoughtworks.com>
2021-02-23 15:34:31 +0800

    add project 3

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Yang Dev Luo on 2021/2/7.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
