138f8f09af589aabf05800d1c677e517db3b3f09
LuoYang <lyyx_0@163.com>
2021-03-24 23:01:13 +0800

    add word scramble project

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Yang Dev Luo on 2021/3/24.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
