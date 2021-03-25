1ea808b1d34f8c22be759094d288cc2273a42fe6
LuoYang <lyyx_0@163.com>
2021-03-25 23:07:19 +0800

    add animations project

---
+++ Animations/Animations/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  Animations
+//
+//  Created by Yang Dev Luo on 2021/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var animationAmount: CGFloat = 1
+    
+    var body: some View {
+        Button("Tap Me") {
+            // self.animationAmount += 1
+        }
+        .padding(40)
+        .background(Color.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .overlay(
+            Circle()
+                .stroke(Color.red)
+                .scaleEffect(animationAmount)
+                .opacity(Double(2 - animationAmount))
+                .animation(
+                    Animation.easeOut(duration: 1)
+                        .repeatForever(autoreverses: false)
+                )
+        )
+        .onAppear {
+            self.animationAmount = 2
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
